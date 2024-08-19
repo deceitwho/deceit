6ee9487019c21d8e7376dd282bcc301fcc3db04f
deceitwho <thedesaimitul@gmail.com>
2024-08-19 13:10:44 +0530

    Add files via upload

---
+++ w.js
@@ -0,0 +1,7 @@
+document.querySelector('.google-btn').addEventListener('click', function() {
+    alert('Google login clicked!');
+});
+
+document.querySelector('.x-btn').addEventListener('click', function() {
+    alert('X login clicked!');
+});
